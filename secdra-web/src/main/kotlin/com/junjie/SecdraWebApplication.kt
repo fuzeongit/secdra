e793a9ef144d7f7ae71c7acafde959a1efc5d3ad
fjj <245142637@qq.com>
2018-11-01 12:44:46 +0800

    .

--- secdra-web/src/main/kotlin/com/junjie/SecdraWebApplication.kt
+++ secdra-web/src/main/kotlin/com/junjie/SecdraWebApplication.kt
@@ -4,8 +4,27 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
+
+//@SpringBootApplication
+//@EnableJpaAuditing
+//class SecdraWebApplication : SpringBootServletInitializer() {
+//    override fun configure(builder: SpringApplicationBuilder): SpringApplicationBuilder {
+//        // 设置启动类,用于独立tomcat运行的入口
+//        return builder.sources(SecdraWebApplication::class.java)
+//    }
+//
+//    companion object {
+//        @JvmStatic
+//        fun main(args: Array<String>) {
+//            runApplication<SecdraWebApplication>(*args)
+//        }
+//    }
+//}
+
+
 @SpringBootApplication
 @EnableJpaAuditing
 class SecdraWebApplication fun main(args: Array<String>) {
     runApplication<SecdraWebApplication>(*args)
 }
+
