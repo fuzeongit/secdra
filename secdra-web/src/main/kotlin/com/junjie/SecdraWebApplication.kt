4d3490ff6bdb6a1a40cb079b6182298cf43e7b1c
fjj <245142637@qq.com>
2018-11-01 14:13:51 +0800

    .

--- secdra-web/src/main/kotlin/com/junjie/SecdraWebApplication.kt
+++ secdra-web/src/main/kotlin/com/junjie/SecdraWebApplication.kt
@@ -1,30 +1,32 @@
 package com.junjie
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
-
-
-//@SpringBootApplication
-//@EnableJpaAuditing
-//class SecdraWebApplication : SpringBootServletInitializer() {
-//    override fun configure(builder: SpringApplicationBuilder): SpringApplicationBuilder {
-//        // 设置启动类,用于独立tomcat运行的入口
-//        return builder.sources(SecdraWebApplication::class.java)
-//    }
-//
-//    companion object {
-//        @JvmStatic
-//        fun main(args: Array<String>) {
-//            runApplication<SecdraWebApplication>(*args)
-//        }
-//    }
-//}
 
 
 @SpringBootApplication
 @EnableJpaAuditing
-class SecdraWebApplication fun main(args: Array<String>) {
-    runApplication<SecdraWebApplication>(*args)
+class SecdraWebApplication : SpringBootServletInitializer() {
+    override fun configure(builder: SpringApplicationBuilder): SpringApplicationBuilder {
+        // 设置启动类,用于独立tomcat运行的入口
+        return builder.sources(SecdraWebApplication::class.java)
+    }
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            runApplication<SecdraWebApplication>(*args)
+        }
+    }
 }
+
+
+//@SpringBootApplication
+//@EnableJpaAuditing
+//class SecdraWebApplication fun main(args: Array<String>) {
+//    runApplication<SecdraWebApplication>(*args)
+//}
 
