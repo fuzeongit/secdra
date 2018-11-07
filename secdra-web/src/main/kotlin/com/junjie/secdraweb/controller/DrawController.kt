0a9acf502d7d3ff6e258e746e51ba9941c99526b
JunJie Fu <245142637@qq.com>
2018-11-07 23:21:23 +0800

    添加vo

--- secdra-web/src/main/kotlin/com/junjie/secdraweb/controller/DrawController.kt
+++ secdra-web/src/main/kotlin/com/junjie/secdraweb/controller/DrawController.kt
@@ -5,8 +5,14 @@ import com.junjie.secdracore.annotations.CurrentUserId
 import com.junjie.secdraservice.dao.IDrawDao
 import com.junjie.secdraservice.model.Draw
 import com.junjie.secdraservice.service.IDrawService
+import com.junjie.secdraservice.service.IUserService
+import com.junjie.secdraservice.service.UserService
+import com.junjie.secdraweb.vo.DrawVo
+import com.junjie.secdraweb.vo.UserVo
 import com.qiniu.util.StringUtils
+import org.springframework.beans.BeanUtils
 import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageImpl
 import org.springframework.data.domain.Pageable
 import org.springframework.data.web.PageableDefault
 import org.springframework.web.bind.annotation.GetMapping
@@ -14,20 +20,22 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.io.File
+import java.util.ArrayList
 
 @RestController
 @RequestMapping("/draw")
-class DrawController(val drawService: IDrawService, val drawDao: IDrawDao) {
+class DrawController(private val drawService: IDrawService, private val userService: IUserService, val drawDao: IDrawDao) {
     /**
      * 根据标签获取
      */
     @GetMapping("/pagingByTag")
-    fun pagingByTag(name: String?, @PageableDefault(value = 20) pageable: Pageable): Page<Draw> {
-        return if (name != null && !StringUtils.isNullOrEmpty(name)) {
+    fun pagingByTag(name: String?, @PageableDefault(value = 20) pageable: Pageable): Page<DrawVo> {
+        val page = if (name != null && !StringUtils.isNullOrEmpty(name)) {
             drawService.pagingByTag(pageable, name)
         } else {
             drawService.paging(pageable)
         }
+        return getPageVo(page)
     }
 
     /**
@@ -35,24 +43,27 @@ class DrawController(val drawService: IDrawService, val drawDao: IDrawDao) {
      */
     @Auth
     @GetMapping("/pagingBySelf")
-    fun pagingBySelf(@CurrentUserId userId: String, @PageableDefault(value = 20) pageable: Pageable) {
-        drawService.pagingByUserId(pageable, userId, true)
+    fun pagingBySelf(@CurrentUserId userId: String, @PageableDefault(value = 20) pageable: Pageable): Page<DrawVo> {
+        val page = drawService.pagingByUserId(pageable, userId, true)
+        return getPageVo(page)
     }
 
     /**
      * 获取他人
      */
     @GetMapping("/pagingByOthers")
-    fun pagingByOthers(userId: String, @PageableDefault(value = 20) pageable: Pageable) {
-        drawService.pagingByUserId(pageable, userId, false)
+    fun pagingByOthers(userId: String, @PageableDefault(value = 20) pageable: Pageable): Page<DrawVo> {
+        val page = drawService.pagingByUserId(pageable, userId, false)
+        return getPageVo(page)
     }
 
     /**
      * 获取图片
      */
     @GetMapping("/get")
-    fun get(id: String, @CurrentUserId userId: String?): Draw {
-        return drawService.get(id, userId)
+    fun get(id: String, @CurrentUserId userId: String?): DrawVo {
+        val draw = drawService.get(id, userId)
+        return getVo(draw)
     }
 
     /**
@@ -60,8 +71,27 @@ class DrawController(val drawService: IDrawService, val drawDao: IDrawDao) {
      */
     @PostMapping("/update")
     @Auth
-    fun update(@CurrentUserId userId: String, drawId: String, desc: String?, isPrivate: Boolean?): Draw {
-        return drawService.update(userId, drawId, desc!!, isPrivate!!)
+    fun update(@CurrentUserId userId: String, drawId: String, desc: String?, isPrivate: Boolean?): DrawVo {
+        val draw = drawService.update(userId, drawId, desc!!, isPrivate!!)
+        return getVo(draw)
+    }
+
+    private fun getVo(draw: Draw): DrawVo {
+        val user = userService.getInfo(draw.userId!!)
+        val drawVo = DrawVo()
+        val userVo = UserVo()
+        BeanUtils.copyProperties(draw, drawVo)
+        BeanUtils.copyProperties(user, userVo)
+        drawVo.user = userVo
+        return drawVo
+    }
+
+    private fun getPageVo(page: Page<Draw>): Page<DrawVo> {
+        val drawVoList = ArrayList<DrawVo>()
+        for (draw in page.content) {
+            drawVoList.add(getVo(draw))
+        }
+        return PageImpl<DrawVo>(drawVoList, page.pageable, page.totalElements)
     }
 
     @GetMapping("/getName")
---
+++ secdra-web/src/main/kotlin/com/junjie/secdraweb/vo/DrawVo.kt
@@ -0,0 +1,30 @@
+package com.junjie.secdraweb.vo
+
+import com.junjie.secdraservice.model.Tag
+import java.util.*
+
+class DrawVo() {
+    var id: String? = null
+
+    var introduction: String? = null
+
+    var url: String? = null
+
+    var isPrivate: Boolean = false
+
+    var viewAmount: Int = 0
+
+    var likeAmount: Int = 0
+
+    var width: Int = 0;
+
+    var height: Int = 0;
+
+    var tagList: MutableSet<Tag>? = null
+
+    var user: UserVo? = null
+
+    var createDate: Date = Date()
+
+    var modifiedDate: Date = Date()
+}
