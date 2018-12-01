5ee88bdee2a12de7ba64baf3b3d1b24df17aefbf
JunJie Fu <245142637@qq.com>
2018-12-02 01:18:29 +0800

    初始化图片接口

--- secdra-admin/src/main/kotlin/com/junjie/secdraadmin/controller/DrawController.kt
+++ secdra-admin/src/main/kotlin/com/junjie/secdraadmin/controller/DrawController.kt
@@ -15,30 +15,40 @@ import javax.imageio.ImageIO
 @RequestMapping("draw")
 class DrawController(private var drawService: IDrawService) {
     @PostMapping("/init")
-    fun init(path: String, name: String? = "小可爱", introduction: String? = "这是一张很好看的图片，这是我从p站上下载回来的，侵删！"): Boolean {
+    fun init(path: String): Any {
+        var i = 0
+        val map = HashMap<String,Array<String>>()
         try {
-            val file = File(path)
-            val fileNameList = file.list()
-            for (fileName in fileNameList) {
-                if (fileName.endsWith(".png") || fileName.endsWith(".jpg")) {
-                    val picture = File("$path/$fileName")
-                    val read = ImageIO.read(FileInputStream(picture))
-                    val draw = Draw()
-                    draw.userId = "402880e566ddba740166ddbce0b70000"
-                    draw.width = read.width.toLong()
-                    draw.height = read.height.toLong()
-                    draw.url = fileName
-                    draw.name = name
-                    draw.introduction = introduction
-                    val tag = Tag()
-                    tag.name = name
-                    val tagList = HashSet<Tag>()
-                    tagList.add(tag)
-                    draw.tagList = tagList
-                    drawService.save(draw)
+            val type = File(path)
+            val typeNameList = type.list()
+            for (typeName in typeNameList) {
+                val typePath = "$path/$typeName"
+                val file = File(typePath)
+                val fileNameList = file.list()
+                map[typeName] = fileNameList
+                for(fileName in fileNameList){
+                    if (fileName.endsWith(".png") || fileName.endsWith(".jpg")) {
+                        val picture = File("$typePath/$fileName")
+                        val read = ImageIO.read(FileInputStream(picture))
+                        val draw = Draw()
+                        draw.userId = "402880e566ddba740166ddbce0b70000"
+                        draw.width = read.width.toLong()
+                        draw.height = read.height.toLong()
+                        draw.url = fileName
+                        draw.name = typeName
+                        draw.introduction = "这是一张很好看的图片，这是我从p站上下载回来的，侵删！"
+                        val tag = Tag()
+                        tag.name = typeName
+                        val tagList = HashSet<Tag>()
+                        tagList.add(tag)
+                        draw.tagList = tagList
+                        drawService.save(draw)
+                    }
+                    i++
                 }
             }
-            return true
+            println(i)
+            return  map
         } catch (e: Exception) {
             println(e.message)
             throw e
