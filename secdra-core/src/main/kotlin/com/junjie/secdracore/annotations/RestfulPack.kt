309eec2b73abd315003f9f473084701d9a06c1b1
fjj <245142637@qq.com>
2019-07-30 18:06:00 +0800

    清理依赖

---
+++ secdra-core/src/main/kotlin/com/junjie/secdracore/annotations/RestfulPack.kt
@@ -0,0 +1,10 @@
+package com.junjie.secdracore.annotations
+
+
+/**
+ * 标识需要包装的接口
+ */
+@Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.RUNTIME)
+@MustBeDocumented
+annotation class RestfulPack
