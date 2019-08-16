069cd54c1310384da5ea73a11e56360967c21298
fjj <245142637@qq.com>
2019-08-16 11:27:49 +0800

    添加接口文档

--- secdra-web/src/main/kotlin/com/junjie/SecdraWebApplication.kt
+++ secdra-web/src/main/kotlin/com/junjie/SecdraWebApplication.kt
@@ -8,26 +8,27 @@ import org.springframework.cache.annotation.EnableCaching
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 import org.springframework.scheduling.annotation.EnableAsync
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
-
+import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 
 @EnableJpaAuditing
 @EnableCaching
 @EnableAsync
 @EnableWebSocketMessageBroker
+@EnableSwagger2
 //@EnableScheduling
 @SpringBootApplication
 class SecdraWebApplication : SpringBootServletInitializer() {
     override fun configure(builder: SpringApplicationBuilder): SpringApplicationBuilder {
         // 设置启动类，用于独立tomcat运行的入口
-        System.setProperty("es.set.netty.runtime.available.processors","false")
+        System.setProperty("es.set.netty.runtime.available.processors", "false")
         return builder.sources(SecdraWebApplication::class.java)
     }
 
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-            System.setProperty("es.set.netty.runtime.available.processors","false")
+            System.setProperty("es.set.netty.runtime.available.processors", "false")
             runApplication<SecdraWebApplication>(*args)
         }
     }
